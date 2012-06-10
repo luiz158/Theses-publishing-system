479482c2da73662ed64f2b5cc1868e626f640e7e
Jakub Čecháček <pseudo.em@gmail.com>
2012-06-10 14:20:49 +0200

    refactoring

---
+++ src/main/java/cz/muni/fi/pv243/tps/security/SecurityRoles.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pv243.tps.security;
+
+/**
+ * @author <a href="mailto:pseudo.em@gmail.com">Jakub Cechacek</a>.
+ */
+public enum SecurityRoles {
+    ADMIN,
+    SUPERVISOR,
+    STUDENT,
+}
