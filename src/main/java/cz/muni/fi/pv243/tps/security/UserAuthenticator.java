240e05f7e8cb9b533f6e70fd0e8a15815cca4078
Jakub Čecháček <pseudo.em@gmail.com>
2012-06-15 12:46:11 +0200

    Bug fix in user Authentication - @Admin and @Supervisor should work now

--- src/main/java/cz/muni/fi/pv243/tps/security/UserAuthenticator.java
+++ src/main/java/cz/muni/fi/pv243/tps/security/UserAuthenticator.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pv243.tps.security;
 
+import cz.muni.fi.pv243.tps.domain.User;
 import cz.muni.fi.pv243.tps.ejb.UserManager;
 import cz.muni.fi.pv243.tps.exceptions.InvalidUserIdentityException;
 import org.jboss.seam.international.status.Messages;
@@ -31,10 +32,9 @@ public class UserAuthenticator extends BaseAuthenticator {
         );
 
         try {
-            userManager.getUserByUserIdentity(userIdentity);
-
+            User user =  userManager.getUserByUserIdentity(userIdentity);
             setStatus(AuthenticationStatus.SUCCESS);
-            setUser(userIdentity);
+            setUser(user.getUserIdentity());
         } catch (InvalidUserIdentityException e) {
             messages.error("Authentication failed. Incorrect username or password.");
             setStatus(AuthenticationStatus.FAILURE);
